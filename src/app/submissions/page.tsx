1667b5d84933fbee8f7ed6e8d42af35af2e6b3a9
YusufSyam <muhyusufsyamsyam@gmail.com>
2025-07-14 19:57:42 +0800

    feat: add dummy submission table

--- src/app/submissions/page.tsx
+++ src/app/submissions/page.tsx
@@ -1,12 +1,28 @@
-"use client"
+"use client";
 
-import React from 'react';
+import HeaderText1 from "@/components/HeaderText1.component";
+import MyTable, { IMyTableColumn } from "@/components/MyTable.component";
+import { dummyLeaderboard, dummySubmissions } from "@/utils/constants/dummies.const";
+import { Stack } from "@mantine/core";
+import React from "react";
 
 export interface ISubmissionPage {}
 
-const SubmissionPage: React.FC<ISubmissionPage> = ({ }) => {
-    return (
-        <></>
-    )
-}
+const columns: IMyTableColumn[] = [
+  { key: "number", label: "No", type: "number" },
+  { key: "subject", label: "Subject", type: "text" },
+  { key: "duration", label: "Duration", type: "duration" },
+  { key: "dateSubmitted", label: "Data Submitted", type: "text" },
+  { key: "score", label: "Score", type: "number" },
+  { key: "rank", label: "Rank", type: "number" },
+];
+
+const SubmissionPage: React.FC<ISubmissionPage> = ({}) => {
+  return (
+    <Stack>
+      <HeaderText1 title="Submissions" />
+      <MyTable columns={columns} data={dummySubmissions} />
+    </Stack>
+  );
+};
 export default SubmissionPage;
