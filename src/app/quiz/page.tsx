3fec0355d6355f7036120eef1350eee2a4bb1a35
YusufSyam <muhyusufsyamsyam@gmail.com>
2025-07-14 18:37:48 +0800

    feat: add yup and quiz submit form

--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -1,10 +1,79 @@
-import React from 'react';
+"use client";
+
+import HeaderText1 from "@/components/HeaderText1.component";
+import { QUIZ_SUBJECT } from "@/utils/constants/quizSubject.const";
+import { Button, Select, Stack, TextInput } from "@mantine/core";
+import { useForm, yupResolver } from "@mantine/form";
+import React from "react";
+import * as yup from 'yup'
 
 export interface IQuizPage {}
 
-const QuizPage: React.FC<IQuizPage> = ({ }) => {
-    return (
-        <></>
-    )
-}
+const schema = yup.object().shape({
+  subject: yup.string().required("Subject is required"),
+  score: yup
+    .number()
+    .min(0, "Minimal 0")
+    .max(100, "Maksimal 100")
+    .required("Score wajib diisi"),
+});
+
+const QuizPage: React.FC<IQuizPage> = ({}) => {
+  const form = useForm({
+    validate: yupResolver(schema),
+    initialValues: {
+      subject: "",
+      score: "",
+    },
+  });
+
+  const handleSubmit = async (values: typeof form.values) => {
+    console.log(values, 'values')
+    // try {
+    //   const res = await fetch("/api/submit", {
+    //     method: "POST",
+    //     headers: { "Content-Type": "application/json" },
+    //     body: JSON.stringify(values),
+    //   });
+
+    //   if (res.ok) {
+    //     alert("Berhasil disubmit!");
+    //     form.reset();
+    //   } else {
+    //     alert("Gagal submit!");
+    //   }
+    // } catch (err) {
+    //   console.error(err);
+    //   alert("Terjadi kesalahan.");
+    // }
+  };
+
+  return (
+    <Stack>
+      <HeaderText1
+        title="Quiz"
+        subTitle="Submit hasil quiz anda dan dapatkan skor yang sesuai"
+      />
+      <form onSubmit={form.onSubmit(handleSubmit)} className="space-y-4">
+        <Select
+          label="Subject"
+          placeholder="Pilih subject"
+          data={QUIZ_SUBJECT}
+          {...form.getInputProps("subject")}
+        />
+
+        <TextInput
+          label="Score"
+          placeholder="Masukkan skor (0–100)"
+          type="number"
+          {...form.getInputProps("score")}
+        />
+
+        <Button type="submit" fullWidth>
+          Submit
+        </Button>
+      </form>
+    </Stack>
+  );
+};
 export default QuizPage;
---
+++ src/utils/constants/quizSubject.const.tsx
@@ -0,0 +1,11 @@
+export const QUIZ_SUBJECT: string[]= [
+    "Astronomi",
+    "Biologi",
+    "Ekonomi",
+    "Fisika",
+    "Geografi",
+    "Informatika",
+    "Kebumian",
+    "Kimia",
+    "Matematika"
+]
