88c485c17d26fe3be9ef543086377bea0d354837
YusufSyam <muhyusufsyamsyam@gmail.com>
2025-07-16 02:32:33 +0800

    feat: add axios instance

---
+++ src/lib/axiosInstance.ts
@@ -0,0 +1,11 @@
+import { BASE_URL } from '@/utils/constants/api.const';
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export default axiosInstance;
